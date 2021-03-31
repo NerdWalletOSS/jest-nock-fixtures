f89a934cca710bf5fff56fcb5f85fd061f8db41b
Benjamin Allen <ballen@nerdwallet.com>
2021-03-31 15:14:28 -0700

    clean up commented code in test file

--- test/implementation.test.js
+++ test/implementation.test.js
@@ -8,7 +8,6 @@ const TEST_URL = 'http://worldclockapi.com/api/json/utc/now';
 
 describe('implementation', () => {
   it('should not allow external requests in CI', async () => {
-    // const res = await fetch(TEST_URL);
     let res = await fetch(TEST_URL);
     const json = await res.json();
     expect(json).toMatchSnapshot();
@@ -17,41 +16,28 @@ describe('implementation', () => {
   });
 
   it.skip('Hi here is another', async () => {
-    // const res = await fetch('http://worldclockapi.com/api/json/est/now');
     const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    // https://jsonplaceholder.typicode.com/todos/1
     const json = await res.json();
     expect(json).toMatchSnapshot();
   });
 
   it('AND here is another', async () => {
-    // const res = await fetch('http://worldclockapi.com/api/json/est/now');
     const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    // https://jsonplaceholder.typicode.com/todos/1
     const json = await res.json();
     expect(json).toMatchSnapshot();
   });
 
   it('AND YET here is another', async () => {
     const res = await fetch('http://worldclockapi.com/api/json/est/now');
-    // const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    // https://jsonplaceholder.typicode.com/todos/1
     const json = await res.json();
     expect(json).toMatchSnapshot();
   });
   it('AND YET here is another', async () => {
     const res = await fetch('http://worldclockapi.com/api/json/est/now');
-    // const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    // https://jsonplaceholder.typicode.com/todos/1
     const json = await res.json();
     expect(json).toMatchSnapshot();
   });
   it('has no requests', async () => {
-    // const res = await fetch('http://worldclockapi.com/api/json/est/now');
-    // // const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    // // https://jsonplaceholder.typicode.com/todos/1
-    // const json = await res.json();
-    // expect(json).toMatchSnapshot();
     expect(2).toBe(2);
   });
 });
